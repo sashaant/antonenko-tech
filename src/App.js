4aba6922cb5dbd259d9bd0f54ad61bbb3e18d8d9
sashaant <antonenko.qa@gmail.com>
2023-02-07 02:37:38 +0200

    retern deleted  app file

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Card from './componets/Card';
+import Footer from './componets/Footer';
+import Navbar from './componets/Navbar';
+import Foto from './componets/Foto';
+
+
+// import Mailto from './componets/Mailto';
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Foto/>
+      <Card/>
+      <DataFromApi/>
+      {/* <Mailto>Mail me!</Mailto> */}
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
