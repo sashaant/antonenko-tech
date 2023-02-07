7585db281a9be7e6d636490ab7f0418893407ac7
sashaant <antonenko.qa@gmail.com>
2023-02-07 16:07:21 +0200

    update Google Analytic v1.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import ReactGA from 'react-ga';
+
 import Card from './componets/Card';
 import Footer from './componets/Footer';
 import Navbar from './componets/Navbar';
 import Foto from './componets/Foto';
 import RunningLine from './componets/RunningLine';
 
-
+const TRACKING_ID = "G-F8CE2RP8XW";
+ReactGA.initialize(TRACKING_ID);
 
 const App = () => {
   return (
