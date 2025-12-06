a2da3db1b3e41974302f141365e776ed69998fd6
Oleksandr Antonenko <41590098+sashaant@users.noreply.github.com>
2025-12-06 18:13:51 +0200

    new_header_1.0 (#4)

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import Card from '../componets/Card';
 import Footer from '../componets/Footer';
-
-
 import DarkModeSwitch from '../componets/DarkModeSwitch';
 import Header from '../componets/Header';
-import HeroComponent from '../componets/HeroComponent';
+
 
 
 const MainPage = () => {
  
     return (
         <div>
-        <Header/>
-
-        <DarkModeSwitch />
-        <HeroComponent/>
-             
-        <Card/>
-        <Footer/>
+            <Header/>
+            <DarkModeSwitch />    
+            <Card/>
+            <Footer/>
         </div>
     );
     }
